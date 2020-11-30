137bd89e2f0905b4d1618bc4a1f8882bf7ac614f
Marcel Wege <mwege@byte5.de>
2020-11-30 12:37:30 +0100

    Add Auth facades

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('startpage');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
