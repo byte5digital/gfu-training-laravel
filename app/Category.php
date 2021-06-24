fb62f5ceb2a06b309f96fa08e7735568257d3967
Marlon Basten <mbasten@byte5.de>
2021-06-24 09:37:36 +0200

    feat: Updated to laravel 8.x feat: Updated seeders feat: Updated factories

--- app/Category.php
+++ app/Category.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -30,6 +31,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Category extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
     protected $fillable = ['name'];
--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Category;
 use App\Article;
 use Illuminate\Database\Seeder;
